484efba2b2fe9da8976bfd63657f2be329657912
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-10-03 00:33:58 -0300

    Implementando na classe ChessMatch um método que coloca uma nova peça em alguma posição, para ao ser chamado no initialSetup() podermos passar como parâmetro coluna e linha nas coordenadas do Xadrez, para isso esse método usa o placePiece() do board mas na hora de passar o parâmetro position, passa como objeto do tipo ChessPosition convertendo-o com o método toPosition()

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -23,10 +23,16 @@ public class ChessMatch {
 		}
 		return mat;
 	}
+
+	// criando um método que vai usar o placePiece() já passando a posição como segundo parâmetro convertido, criando um ChessPosition no parâmetro e chamando o método toPosition
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+	}
 	
 	private void initialSetup() {
-		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-		board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
-		board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
+		// agora chamando o método placeNewPiece, que poderei passar os parâmetros no sistema de coordenadas do xadrez e passando uma peça específica como último parâmetro
+		this.placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+		this.placeNewPiece('e', 8, new King(board, Color.BLACK));
+		this.placeNewPiece('e', 1, new King(board, Color.WHITE));
 	}
 }
